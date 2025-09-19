7f29c4e5a9dad04bb70348da7a4bba038d29a010
Maycon Marigo <67290959+MayconMarigo@users.noreply.github.com>
2025-09-19 12:00:30 -0300

    Update admin.js

--- src/database/query/admin.js
+++ src/database/query/admin.js
@@ -145,12 +145,12 @@ const getAllCalls = async (startDate, endDate) => {
 
   const [durationInMinutes] = await sequelize.query(`
     SELECT 
-      COUNT(TIME_FORMAT(SEC_TO_TIME(
-      GREATEST(
-          CEIL(TIMESTAMPDIFF(SECOND, c.startTime, c.endTime) / 60), 
-        1
-        ) * 60
-      ), '%i')) AS minutes_count
+      COALESCE(SUM(
+          GREATEST(
+              CEIL(TIMESTAMPDIFF(SECOND, c.startTime, c.endTime) / 60),
+              1
+          )
+      ), 0) AS minutes_count
       from calls c
       WHERE
         c.isSocketConnection = 1
